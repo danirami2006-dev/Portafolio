c8ff8308ee5f6605729700bc1dd65bcb64d60adc
Daniela Quintero Ramirez <dani.rami2006@gmail.com>
2026-09-20 02:30:38 -0500

    Update ilustraciones.script.js

--- JS/ilustraciones.script.js
+++ JS/ilustraciones.script.js
@@ -1,14 +1,18 @@
-<script>
-    function abrirModal(rutaImagen) {
-        const modal = document.getElementById('modal-visor');
-        const imgModal = document.getElementById('img-modal-ampliada');
-        
+// Función para abrir la imagen en pantalla completa con el fondo lila
+function abrirModal(rutaImagen) {
+    const modal = document.getElementById('modal-visor');
+    const imgModal = document.getElementById('img-modal-ampliada');
+    
+    if (modal && imgModal) {
         imgModal.src = rutaImagen;
         modal.style.display = 'flex';
     }
+}
 
-    function cerrarModal() {
-        const modal = document.getElementById('modal-visor');
+// Función para cerrar la imagen
+function cerrarModal() {
+    const modal = document.getElementById('modal-visor');
+    if (modal) {
         modal.style.display = 'none';
     }
-</script>
+}
